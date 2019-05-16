a95c6071c16a143b26766e14066b6dde4979f00a
“gurps” <g_malhi999@hotmail.com>
2019-05-16 16:05:29 +0100

    added several files for the to do list

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("registrar");
+const input = form.querySelector('input');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(input.value);
+})
+
+
+
